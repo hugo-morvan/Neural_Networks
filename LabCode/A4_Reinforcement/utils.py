ace3b317a92a6cb483cb3b8f40eaba1dc6d66b80
Hugo Morvan <Hugo.Morvan@coyotes.usd.edu>
2024-02-29 20:17:00 +0100

    passed lab 4

--- LabCode/A4_Reinforcement/utils.py
+++ LabCode/A4_Reinforcement/utils.py
@@ -4,25 +4,25 @@ from matplotlib import pyplot as plt
 
 
 def getpolicy(Q):
-    """ Get best policy matrix from the Q-matrix.
-    You have to implement this function yourself. It is not necessary to loop
-    in order to do this, and looping will be much slower than using matrix
-    operations. It's possible to implement this in one line of code.
-    """
-
-    P = None
-
-    return P
-
-
-def getvalue(Q):
-    """ Get best value matrix from the Q-matrix.
+    """ Get best policy (which action to take in each state) matrix from the Q-matrix.
     You have to implement this function yourself. It is not necessary to loop
     in order to do this, and looping will be much slower than using matrix
     operations. It's possible to implement this in one line of code.
     """
     
-    V = None
+    P = np.argmax(Q, axis = 2)
+
+    return P
+
+
+def getvalue(Q):
+    """ Get best value (the value of being in the state given a policy,)) matrix from the Q-matrix.
+    You have to implement this function yourself. It is not necessary to loop
+    in order to do this, and looping will be much slower than using matrix
+    operations. It's possible to implement this in one line of code.
+    """
+    
+    V = np.max(Q, axis = 2)
 
     return V
 
